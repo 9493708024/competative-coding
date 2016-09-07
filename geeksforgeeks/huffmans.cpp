d6d6d63362bf69ebd0b2914198a6599e63e24b9c
Rajith Jarang <rajithjarang@gmail.com>
2016-09-08 03:53:13 +0530

    Update huffmans.cpp

--- geeksforgeeks/huffmans.cpp
+++ geeksforgeeks/huffmans.cpp
@@ -88,7 +88,7 @@ inline void ins(std::string &str)
 
 //------------------FASTER OUTPUT FUNCTIONS-----------------//
 
-char sp;
+char sp='\n';
 
 inline  void outi (int n){
     register int x = n;
